6f82a4e2768163a1d75e34993d865fd37a40323b
abhishek <abhishek.km2010@gmail.com>
2026-06-24 11:18:57 +0530

    updates

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,5 @@
 import { Component, signal } from '@angular/core';
-import { form, FormField } from '@angular/forms/signals';
+import { form, FormField, minLength, required } from '@angular/forms/signals';
 import { RouterOutlet } from '@angular/router';
 
 export interface LoginFormData {
@@ -17,11 +17,15 @@ export class App {
   protected readonly title = signal('myapp');
 
   loginFormModel = signal<LoginFormData>({
-    email: '',
-    password: ''
+    email: 'test',
+    password: 'test'
   })
 
-  loginForm = form(this.loginFormModel)
+  loginForm = form(this.loginFormModel, (login) => {
+    required(login.email),
+    required(login.password),
+    minLength(login.password, 8)
+  })
   
   isEmailValid() {
   }
@@ -29,7 +33,10 @@ export class App {
   onSubmit(event: Event) {
     event.preventDefault()
     event.stopPropagation()
-    console.log(this.loginForm.email())
+    // console.log(this.loginForm().controlValue())
+    console.log(this.loginForm.password().valid());
+    console.log(this.loginForm.password().errors());
+    console.log(this.loginForm().errors());
   }
 
 }
