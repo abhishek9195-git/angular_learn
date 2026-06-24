bad70bb969288edb2e0feb2f9d09ca8d21689ebb
abhishek <abhishek.km2010@gmail.com>
2026-06-24 10:33:15 +0530

    fix: updates

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,35 @@
 import { Component, signal } from '@angular/core';
+import { form, FormField } from '@angular/forms/signals';
 import { RouterOutlet } from '@angular/router';
+
+export interface LoginFormData {
+  email: string 
+  password: string 
+}
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormField],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('myapp');
+
+  loginFormModel = signal<LoginFormData>({
+    email: '',
+    password: ''
+  })
+
+  loginForm = form(this.loginFormModel)
+  
+  isEmailValid() {
+  }
+
+  onSubmit(event: Event) {
+    event.preventDefault()
+    event.stopPropagation()
+    console.log(this.loginForm.email())
+  }
+
 }
